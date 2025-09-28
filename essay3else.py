09bf0697d1853e3f3846964eeae4226496882e6b
Nickkkssss <nicolasdhminecraft@gmail.com>
2025-09-28 10:38:31 -0300

    chuva code

---
+++ essay3else.py
@@ -0,0 +1,10 @@
+raining = False
+cloudy = True 
+
+if raining:
+    print("You take the umbrella")
+else:
+    if cloudy:
+        print("The weather its cloudy, you should take the umbrella")
+    else:
+        print("You doesnt need to take the umbrella")
