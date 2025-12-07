a2509acdf698bf72323c0f8af8fec1aaeed25eed
digu-d <diguguido13@gmail.com>
2025-12-07 11:30:14 -0300

    Refactor battle logic and improve readability

--- essay4quiz.py
+++ essay4quiz.py
@@ -7,21 +7,25 @@ for _ in range(4):
     acao = input("Ação (espada/cura/raio): ")
 
     if acao == "espada":
-        vida_monstro -= 3
+        vida_monstro -=3
     elif acao == "cura" and cura_disponivel:
-        vida_paladino += 6
-        cura_disponivel = 0
-    elif acao == "raio" and raio_disponivel:
-        vida_monstro -= 6
-        raio_disponivel = 0
+        vida_paladino +=6
+        cura_disponivel=0
+    elif acao == "raio" and cura_disponivel:
+        vida_monstro -=6
+        raio_disponivel=0
+    
 
-    if vida_monstro <= 0:
-        print("Você venceu!")
-        break
+        if vida_monstro <=0:
+            print("Voce venceu")
+            break
 
-    vida_paladino -= 5
-    if vida_paladino <= 0:
-        print("Você perdeu!")
-        break
 
-print("Fim. Vida do Paladino:", vida_paladino, "Vida monstro)
+        print("O monstro atacou")
+        vida_paladino -=5
+        
+        if vida_paladino <=0:
+            print("Voce perdeu")
+            break
+
+print("Vida Paladino: ",vida_paladino,vida_monstro,"Vida monstro: ")
