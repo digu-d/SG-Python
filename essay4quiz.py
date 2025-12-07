2b0c94ca980fb73cecff6a60adb303a6acb1b1ef
digu-d <diguguido13@gmail.com>
2025-12-07 11:09:17 -0300

    Add battle mechanics for Paladino and Monstro
    Implement game logic for Paladino vs. Monstro battle

---
+++ essay4quiz.py
@@ -0,0 +1,27 @@
+vida_paladino = 10
+vida_monstro = 10
+cura_disponivel = 1
+raio_disponivel = 1
+
+for _ in range(4):
+    acao = input("Ação (espada/cura/raio): ")
+
+    if acao == "espada":
+        vida_monstro -= 3
+    elif acao == "cura" and cura_disponivel:
+        vida_paladino += 6
+        cura_disponivel = 0
+    elif acao == "raio" and raio_disponivel:
+        vida_monstro -= 6
+        raio_disponivel = 0
+
+    if vida_monstro <= 0:
+        print("Você venceu!")
+        break
+
+    vida_paladino -= 5
+    if vida_paladino <= 0:
+        print("Você perdeu!")
+        break
+
+print("Fim. Vida do Paladino:", vida_paladino, "Vida monstro)
